90c9fcb505a134e2190e204a96642369d8860c07
Het Pandya <55797177+thehetpandya@users.noreply.github.com>
2020-08-09 20:23:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask import request
 import torch
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 
+# name = "twmkn9/albert-base-v2-squad2"
+# name = "distilbert-base-uncased-distilled-squad"
 name = "mrm8488/bert-small-finetuned-squadv2"
 
 tokenizer = AutoTokenizer.from_pretrained(name,)
